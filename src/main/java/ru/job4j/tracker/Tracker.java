58165b074712c29f8b339101c1b75f8e2437c119
Arrcady555 <parfenov7233@gmail.com>
2021-11-21 21:08:37 +0300

    7. Метод удаления заявки Tracker.delete [#211749]-2

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -71,7 +71,7 @@ public class Tracker {
             return false;
         } else {
             items[indexOf(id)] = null;
-            System.arraycopy(items, index + 1, items, index,size - index -1);
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
             return true;
