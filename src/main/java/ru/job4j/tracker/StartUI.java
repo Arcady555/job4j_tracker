cf3fc574a03693c2c9129e9bf199c40598144ba5
Arrcady555 <parfenov7233@gmail.com>
2021-11-24 14:14:38 +0300

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745 #231891]-7

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,3 @@
-
-
 package ru.job4j.tracker;
 
 import java.util.Scanner;
@@ -7,7 +5,6 @@ import java.util.Scanner;
 public class StartUI {
 
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
         new StartUI().init(scanner, tracker);
@@ -19,27 +16,26 @@ public class StartUI {
             showMenu();
             System.out.print("Select: ");
             int select = Integer.parseInt(scanner.nextLine());
-            if (select >= 0 & select <= 5) {
-                selection(select, scanner, tracker);
+            if (select == 0) {
+                System.out.println("=== Create a new Item ===");
+                System.out.print("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 1) {
+                System.out.println("=== Show all items ===");
+                Item[] items = tracker.findAll();
+                if (items.length > 0) {
+                    for (Item item : items) {
+                        System.out.println(item);
+                    }
+                } else {
+                    System.out.println("Хранилище еще не содержит заявок");
+                }
             } else if (select == 6) {
                 run = false;
             }
-        }
-    }
-
-    private void selection(int select, Scanner scanner, Tracker tracker) {
-        if (select == 0) {
-            ifZero(scanner, tracker);
-        } else if (select == 1) {
-            ifOne(tracker);
-        } else if (select == 2) {
-            ifTwo(scanner, tracker);
-        } else if (select == 3) {
-            ifThree(scanner, tracker);
-        } else if (select == 4) {
-            ifFour(scanner, tracker);
-        } else if (select == 5) {
-            ifFive(scanner, tracker);
         }
     }
 
@@ -52,78 +48,6 @@ public class StartUI {
         System.out.println("Menu:");
         for (int i = 0; i < menu.length; i++) {
             System.out.println(i + ". " + menu[i]);
-        }
-    }
-
-    private void ifZero(Scanner scanner, Tracker tracker) {
-        System.out.println("=== Create a new Item ===");
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        Item item = new Item(name);
-        tracker.add(item);
-        System.out.println("Добавленная заявка: " + item);
-    }
-
-    private void ifOne(Tracker tracker) {
-        System.out.println("===Show all items");
-        Item[] items = tracker.findAll();
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Хранилище еще не содержит заявок");
-        }
-    }
-
-    private void ifTwo(Scanner scanner, Tracker tracker) {
-        System.out.println("=== Edit item ===");
-        System.out.print("Enter id: ");
-        int id = Integer.parseInt(scanner.nextLine());
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        Item item = new Item(name);
-        if (tracker.replace(id, item)) {
-            System.out.println("Заявка изменена успешно.");
-        } else {
-            System.out.println("Ошибка замены заявки.");
-        }
-    }
-
-    private void ifThree(Scanner scanner, Tracker tracker) {
-        System.out.println("=== Delete item ===");
-        System.out.print("Enter id: ");
-        int id = Integer.parseInt(scanner.nextLine());
-        if (tracker.delete(id)) {
-            System.out.println("Заявка удалена успешно.");
-        } else {
-            System.out.println("Ошибка удаления заявки.");
-        }
-    }
-
-    private void ifFour(Scanner scanner, Tracker tracker) {
-        System.out.println("=== Find item by id ===");
-        System.out.print("Enter id: ");
-        int id = Integer.parseInt(scanner.nextLine());
-        Item item = tracker.findById(id);
-        if (item != null) {
-            System.out.println(item);
-        } else {
-            System.out.println("Заявка с введенным id: " + id + " не найдена.");
-        }
-    }
-
-    private void ifFive(Scanner scanner, Tracker tracker) {
-        System.out.println("=== Find items by name ===");
-        System.out.print("Enter name: ");
-        String name = scanner.nextLine();
-        Item[] items = tracker.findByName(name);
-        if (items.length > 0) {
-            for (Item item : items) {
-                System.out.println(item);
-            }
-        } else {
-            System.out.println("Заявки с именем: " + name + " не найдены.");
         }
     }
 }
