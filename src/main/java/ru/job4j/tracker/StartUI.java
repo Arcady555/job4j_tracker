cd2d7a56f8128a2f4ed8617d98d216077024a3a5
Arrcady555 <parfenov7233@gmail.com>
2021-11-24 10:19:07 +0300

    2.4. Реализация класса StartUI. Вывод всех заявок [#500745 #231891]-6

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class StartUI {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Tracker tracker = new Tracker();
         new StartUI().init(scanner, tracker);
