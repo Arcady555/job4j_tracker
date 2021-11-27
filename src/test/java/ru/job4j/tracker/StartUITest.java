0cc44835881fb7f518975c6b00c4fa30c7a29cd8
Arrcady555 <parfenov7233@gmail.com>
2021-11-27 20:34:56 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -7,32 +7,25 @@ import static org.junit.Assert.*;
 
 public class StartUITest {
 
-  /*  @Test
-    public void whenCreateItem() {
-        Input in = new StubInput(
-                new String[] {"0", "Item name", "1"}
-        );
-        Tracker tracker = new Tracker();
-        UserAction[] actions = {
-                new CreateAction(),
-                new Exit()
-        };
-        new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findAll()[0].getName(), is("Item name"));
-    }
-
     @Test
-    public void whenDeleteItem() {
-        Tracker tracker = new Tracker();
-        Item item = tracker.add(new Item("Deleted item"));
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(item.getId()), "1"}
+                new String[] {"8", "0"}
         );
-        UserAction[] actions = {
-                new DeleteAction(),
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
                 new Exit()
         };
-        new StartUI().init(in, tracker, actions);
-        Assert.assertNull(tracker.findById(item.getId()));
-    } */
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString(), is(
+                        "Menu:" + ln
+                                + "0. Exit" + ln
+                                + "Wrong input, you can select: 0 .. 0" + ln
+                                + "Menu:" + ln
+                                + "0. Exit" + ln
+                )
+        );
+    }
 }
