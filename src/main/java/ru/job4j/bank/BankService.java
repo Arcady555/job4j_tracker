b56145fe01a7bdb45a3affefb25834ba238895e4
Arrcady555 <parfenov7233@gmail.com>
2021-12-07 11:21:40 +0300

    3. Банковские переводы [#10038 #236368]-3

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -9,8 +9,7 @@ public class BankService {
     private final Map<User, List<Account>> users = new HashMap<>();
 
     public void addUser(User user) {
-        List<Account> accountList = new ArrayList<>();
-        users.putIfAbsent(user, accountList);
+        users.putIfAbsent(user, new ArrayList<>());
     }
 
     public void addAccount(String passport, Account account) {
