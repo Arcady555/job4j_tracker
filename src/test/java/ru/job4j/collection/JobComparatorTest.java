4e1bfbb6160ea7bcb9aec5df99f2ad8b837652f0
Arrcady555 <parfenov7233@gmail.com>
2021-12-09 16:17:14 +0300

    2. Комбинированный компаратор [#10036 #237260]-3

--- src/test/java/ru/job4j/collection/JobComparatorTest.java
+++ src/test/java/ru/job4j/collection/JobComparatorTest.java
@@ -9,7 +9,9 @@ import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.*;
 
 public class JobComparatorTest {
-
+    // Тест-образец в уроке,
+    // не стал трогать
+    // :
     @Test
     public void whenComparatorByNameAndPriority() {
         Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
@@ -20,7 +22,14 @@ public class JobComparatorTest {
         assertThat(rsl, lessThan(0));
     }
 
-    @Test
+    // Из условия:
+    // 1. ....создайте 4 компаратора:
+    // - по возрастанию имени;
+    // - по возрастанию приоритета;
+    // - по убыванию имени;
+    // - по убыванию приоритета.
+    //2. Напишите тесты, проверяющие отдельно поведение компараторов.
+    // :
     public void whenSortByName() {
         Comparator<Job> name = new SortByNameJob();
         int rsl = name.compare(
@@ -60,13 +69,25 @@ public class JobComparatorTest {
         assertThat(rsl, greaterThan(0));
     }
 
+    // 3. Напишите тесты, проверяющие поведение комбинированных компараторов.
+    //:
+    @Test
+    public void whenComparatorByNameAndPriorityOfMe() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Impl task", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+
     @Test
     public void whenComparatorByPriorityAndName() {
         Comparator<Job> cmpNamePriority = new JobDescByPriority().thenComparing(new JobDescByName());
         int rsl = cmpNamePriority.compare(
                 new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("Fix bug", 0)
         );
-        assertThat(rsl, greaterThan(0));
+        assertThat(rsl, lessThan(0));
     }
 }
