548ac9dd65bbd531fe35ced9299304c7ae8a4470
Arrcady555 <parfenov7233@gmail.com>
2021-12-16 14:31:43 +0300

    0. Stream API [#58166 #240625]-2

--- src/main/java/ru/job4j/stream/StreamTask.java
+++ src/main/java/ru/job4j/stream/StreamTask.java
@@ -3,12 +3,11 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamTask {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
-        List<Integer> even = list.stream().filter(num -> num % 2 == 0).toList();
-        even.forEach(System.out::println);
+        List<Integer> list = new ArrayList<>(Arrays.asList(-5, -4, -3, 0, 4, 5, 6, 7, 8, 9));
+        List<Integer> positive = list.stream().filter(num -> num > 0).toList();
+        positive.forEach(System.out::println);
     }
 }
